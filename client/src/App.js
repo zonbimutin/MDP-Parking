a2056d8347b8b638c0cfc1bcf41967fcabeaeb0a
Felipe Alarcon <zonbimutin@gmail.com>
2020-12-28 18:54:34 +0100

    add apollo config

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { ApolloProvider } from "@apollo/client";
+import client from "./config/apollo";
 
 export default function App() {
   return (
-    <div className="app">
-      <h1>Hello World!</h1>
-    </div>
+    <ApolloProvider client={ client }>
+      <div className="app">
+        <h1>Hello World!</h1>
+      </div>
+    </ApolloProvider>
   );
 }
 

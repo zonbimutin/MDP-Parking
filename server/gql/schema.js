8d307430657862ea8c2f46d7f923e909d81e5f36
Felipe Alarcon <zonbimutin@gmail.com>
2020-12-28 20:13:07 +0100

    update stable project

--- server/gql/schema.js
+++ server/gql/schema.js
@@ -1,4 +1,4 @@
-import { gql } from "apollo-server";
+const { gql } = require('apollo-server');
 
 const typeDefs = gql`
     type User {
@@ -41,4 +41,4 @@ const typeDefs = gql`
     }
 `;
 
-export default typeDefs;
+module.exports = typeDefs;
